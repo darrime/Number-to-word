0cce1be396e998eec61a7a2c8038bd90b78bb73a
darrime <darrime627@gmail.com>
2022-02-11 18:40:41 +0900

    핵심 로직 구현

--- Core/main.py
+++ Core/main.py
@@ -1,14 +1,28 @@
+import sqlite3
+
 a = []
 
-for i in range(10):
+for i in range(10000):
     a.append(str(i).zfill(4))
 
-dic = ['말괄양이', '삐삐', '너는바보', '개빡치네', '내가CTO?', '바', '사', '아', '자']
+
+conn = sqlite3.connect("C:\\Dev\\Python\\Number-to-word\\kr_korean.db")
+
+cursor = conn.cursor()
+
+cursor.execute('''select * from kr where part='명사' ''')
+words = cursor.fetchall()
 
 
 def number_to_string(number) -> str:
-    uu = a.index(number)
-    return dic[uu]
+    return words[int(number)]
 
-dd = number_to_string('0001')
-print(dd)
+
+
+phone_number = input("전화번호 뒷자리를 입력하세요\n")
+
+
+woo = number_to_string(phone_number)
+print(woo[1])
+#TODO 숫자-단어 매칭 알고리즘을 어떻게 할지
+#TODO 단어 필터링
