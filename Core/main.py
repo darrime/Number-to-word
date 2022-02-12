e6d00bd75590d6f86853b888a877581f763d5973
darrime <darrime627@gmail.com>
2022-02-12 22:13:45 +0900

    File tree modified

--- Core/main.py
+++ Core/main.py
@@ -6,7 +6,7 @@ for i in range(10000):
     a.append(str(i).zfill(4))
 
 
-conn = sqlite3.connect("D:\\Dev\\PythonProject\\Num-to-word\\kr_korean.db")
+conn = sqlite3.connect("kr_korean.db")
 
 cursor = conn.cursor()
 
@@ -31,6 +31,6 @@ phone_number = input("전화번호를 입력하세요\n")
 
 
 woo = number_to_string(phone_number)
-print(woo)
+print('010-' + woo)
 #TODO 숫자-단어 매칭 알고리즘을 어떻게 할지
 #TODO 단어 필터링
